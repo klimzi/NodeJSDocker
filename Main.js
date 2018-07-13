818be4d70c2c99a219462863b409da4468d2cacd
klimzi <35668535+klimzi@users.noreply.github.com>
2018-07-13 15:04:41 +0300

    Create Main.js

---
+++ Main.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+var router = express.Router();
+router.get('/',function(req,res){
+res.json({"error" : false, "message" : "Hello Abhinav!"});
+});
+router.post('/add',function(req,res){
+res.json({"error" : false, "message" : "success", "data" :
+req.body.num1 + req.body.num2});
+});
+app.use('/',router);
+app.listen(80,function(){
+console.log("Listening at PORT 80");
+})
